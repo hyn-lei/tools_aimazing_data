9bb43913410f733fbcdb84d6a9a713823f305b74
hyn.x.lei <hyn.x.lei@gmail.com>
2025-03-25 14:08:53 +0800

    1. 忽略 jobs, employeed 的页面；

--- app/services/web_crawler.py
+++ app/services/web_crawler.py
@@ -71,7 +71,7 @@ class WebCrawler:
             ignore_patterns = [
                 '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.zip', '.rar',
                 'login', 'signin', 'signup', 'register', 'auth',
-                'cart', 'checkout', 'account', 'integrations', 'glossary', 'lesson', 'changelog', 'guide', 'university',
+                'cart', 'checkout', 'account', 'integrations', 'glossary', 'lesson', 'changelog', 'guide', 'university', 'jobs', 'employee'
                 'privacy',
                 'post', 'blog', 'affiliate'
             ]
